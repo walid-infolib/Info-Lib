c933096ca6d46bf4e0941ddae12ef2c38d614e40
sofien Moalla <smoalla@infolib.tn>
2024-02-27 12:44:08 +0100

    [ADD] - Stock Picking Valued

---
+++ stock_picking_valued/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Stock Picking Valued",
+    'author': "Info'Lib",
+    'description': """
+    Valuation of the delivery note report according to the client
+    """,
+    'depends': ['sale_stock'],
+    "data": [
+        "views/stock_picking_views.xml",
+        "views/res_partner_views.xml",
+        "views/report_deliveryslip.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+}
