ee4960d10491ffb4e75734d30b6fb47e17f532ef
sofien Moalla <smoalla@infolib.tn>
2024-05-16 10:50:06 +0100

    [ADD] - Check Tunisian VAT number

---
+++ base_vat_tn/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Tunisian VAT Number Validation",
+    'version': '17.0.0.0.0',
+    "author": "Info'Lib",
+    "website": "https://www.infolib.tn/",
+    'category': 'Accounting/Accounting',
+    'description': """
+Check Tunisian VAT number
+============================================
+
+This Module will allow you to check the tax registration number of your customers and suppliers in accordance with Tunisian law.
+    """,
+    'depends': ['base_vat'],
+    'installable': True,
+    'auto_install': False,
+    "images": [
+        "images/infolib_tn.png",
+        ],
+}
