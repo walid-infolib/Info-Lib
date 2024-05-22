8f13b1f00d3c174772b6dbea89880bb861bb55d2
sofien Moalla <smoalla@infolib.tn>
2024-05-22 13:00:17 +0100

    [ADD] - CountryInfo

---
+++ base_countryinfo/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "CountryInfo",
+    'version': '17.0.0.0.0',
+    "author": "Info'Lib",
+    "website": "https://www.infolib.tn/",
+    'category': 'Localization',
+    'description': """
+Import Country Provinces
+============================================
+    """,
+    'depends': ['base'],
+    "external_dependencies": {"python": ["countryinfo"]},
+    "data": [
+        "views/res_country.xml"
+    ],
+    'installable': True,
+    "license": "LGPL-3",
+    "images": [
+        "images/infolib_tn.png",
+        ],
+}
