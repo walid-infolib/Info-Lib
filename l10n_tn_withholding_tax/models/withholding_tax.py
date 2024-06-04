3917c062d56c32e733e545cd4bac71c19fe624ce
sofien Moalla <smoalla@infolib.tn>
2024-06-04 11:45:39 +0100

    [ADD] -Retenue à la source

---
+++ l10n_tn_withholding_tax/models/withholding_tax.py
@@ -0,0 +1,12 @@
+from odoo import api, models, fields
+
+
+class WithholdingTax(models.Model):
+    _name = "withholding.tax"
+    _description = "Withholding Tax"
+
+    name = fields.Char(string="Name")
+    amount = fields.Float(string="Amount")
+    designation = fields.Char(string="Designation")
+    account_customer_id = fields.Many2one("account.account", string="Customer Account")
+    account_supplier_id = fields.Many2one("account.account", string="Supplier Account")
